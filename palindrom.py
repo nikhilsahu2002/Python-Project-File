a60193022fddbcf9f202c1287e0af1623b7fd196
nikhilsahu2002 <117375980+nikhilsahu2002@users.noreply.github.com>
2023-03-20 21:22:59 +0530

    ommi

--- palindrom.py
+++ palindrom.py
@@ -2,20 +2,27 @@ st = input("Enter The String")
 c=[]
 
 k = st.split()
-print(k) 
+# print(k) 
+# print(c)
 for i in range(0,len(k)):
     j=k[i]
-c = list(j)
-j= c.reverse()
-# print(j)
-for o in range(0,len(k)):
-    n=k[o]
-m = list(n)
-print(c)
-print(m)
+    v =list(j)
+    c = list(j)
+    j= c.reverse()
+    # print(v)
+    for o in range(0,len(k)):
+        n=k[o]  
+    m = list(n) 
+    # print(c)
+    # print(m)
+
+    for g in range(0,len(k)):
+        if(c[g]=='a' or c[g]=='i' or c[g]=='o' or c[g]== 'e' or c[g]=='u' or v[g]=='a' or v[g]=='o' or v[g]=='i' or v[g]=='e' or v[g]=='u'): 
+            print(f"vowel {c[g] or m[g]}")   
+       
 
 for o in range(0,len(n)):
-    if(c[o]==n[o]):
+    if(c[o]==n[o] and v[o]==n[o]):
         flag=0
     else:
         flag=1
