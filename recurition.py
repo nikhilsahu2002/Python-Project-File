b1b508187fba7dfda747dc0cc216c9485cfecac7
nikhilsahu2002 <117375980+nikhilsahu2002@users.noreply.github.com>
2023-04-10 12:15:46 +0530

    comiit

--- recurition.py
+++ recurition.py
@@ -3,7 +3,7 @@
 
 # print(sys.getrecursionlimit())
 
-
+#non -tail recursion
 def fac(n):
     if(n<0):
         return print("not posible")
@@ -16,13 +16,49 @@ def fac(n):
     
 print(f"Factorial is {fac(5)}")
 
-def num(n):
-    
-    if n==0:
-        print(0) 
+# tail recruion
+def fac1(n,a=1):
+    if(n<0):
+        return print("not posible")
+    elif(n==1):
+        return a
+    elif(n==0):
+        return a
     else:
-         (num(n-1))
-         print(n)
+        return  fac1(n-1,n*a)
+    
 
-num(5)
+
+print(f"Factorial is {fac1(5)}")
+
+# def num(n):
+    
+#     if n==0:
+#         print(0) 
+#     else:
+#          (num(n-1))
+#          print(n)
+
+
+# x=num(5)
+
+#fibonacci series
+def feb(n):
+    if(n==0):
+        return 0
+    elif n==1:
+        return 1
+    else:
+        return feb(n-2) + feb(n-1)
+    
+num = int(input("Enter The Range"))
+for i in range(0,num):
+    print(feb(i),end=' ')
+
+
+
+
+
+
+
 
